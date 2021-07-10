a980da3acd19899f76a1b36dddd86bbee7cbb59c
JorgeSantilli <jorgesantilli1@gmail.com>
2021-07-09 21:15:41 -0300

    No puedo mostrar los datos del servicio

--- src/routes/servicios_routes.js
+++ src/routes/servicios_routes.js
@@ -8,9 +8,23 @@ router.get('/', (req, res) => {
 	const sql = 'SELECT * FROM servicios';
 	connection.query(sql, (error, result) => {
 		if (error) {
+			console.log('error al obtener los datos de servicios');
+		} else {
+			res.json(result);
+		}
+	});
+});
+
+router.get('/:id', (req, res) => {
+	const ID_servicios = req.params.id;
+	const sql = 'SELECT * FROM servicios WHERE ID_servicios=?';
+
+	connection.query(sql, [ID_servicios], (error, result) => {
+		if (error) {
 			console.log('error al obtener los datos de servicio');
 		} else {
 			res.json(result);
+			console.log(result[0]);
 		}
 	});
 });
